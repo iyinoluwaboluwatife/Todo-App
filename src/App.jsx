598ec5426da736bdc1f552244aba310c695eb41b
Malomo Iyin-Oluwa <iyinoluwaboluwatife@gmail.com>
2025-06-13 00:28:33 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,46 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MainLayout from './layout/mainlayout.jsx';
 import EditPage from './pages/editpage.jsx';
 import AddTaskPage from './pages/addtaskpage.jsx';
-import ErrorPage from './pages/error404page.jsx';
+import ErrorPage from './pages/error404page.jsx'; 
+import TestErrorPage from './pages/testerrorpage.jsx';
 import HomePage from './pages/homepage.jsx';
 import ViewMorePage, { todoLoader } from './pages/viewmorepage.jsx';
 
 let queryClient = new QueryClient();
 
-function App() {
-  let router = createBrowserRouter([
+let router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: 'edit-task/:id', element: <EditPage />, loader: todoLoader },
+      { path: 'add-task', element: <AddTaskPage /> },
+      { path: 'viewmore-task/:id', element: <ViewMorePage />, loader: todoLoader },
       {
-        path: 'edit-task/:id',
-        element: <EditPage />,
-        loader: todoLoader,
-      },
-      {
-        path: 'add-task',
-        element: <AddTaskPage />,
-      },
-      {
-        path: 'viewmore-task/:id',
-        element: <ViewMorePage />,
-        loader: todoLoader,
+        path: 'trigger-error', 
+        element: <TestErrorPage />,
+        errorElement: <ErrorPage />, 
       },
     ],
   },
   {
     path: '*',
-    element: <ErrorPage />,
+    element: <ErrorPage />, 
   },
 ]);
 
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
 
 export default App;
-
-
-
-
-
