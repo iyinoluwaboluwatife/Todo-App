05a724cd393aac1fe902c77e43a2f8da5a3ea204
Malomo Iyin-Oluwa <iyinoluwaboluwatife@gmail.com>
2025-06-13 10:03:45 +0100

    updated

--- src/pages/testerrorpage.jsx
+++ src/pages/testerrorpage.jsx
@@ -1,3 +1,23 @@
-export default function TestErrorPage() {
-  throw new Error("This is a test error for the error boundary.");
+import { Link } from "react-router-dom";
+import {FaExclamationTriangle} from 'react-icons/fa';
+
+function TestErrorPage() {
+    
+    return(
+        <>
+        <section className="errorpage">
+            <section className="error">
+                <FaExclamationTriangle className="errortriangle"></FaExclamationTriangle>
+                <h2>Oops!</h2>
+                <p>Page not found</p>
+                <Link to='/'>Go back</Link>
+            </section>
+        </section>
+        
+        </>
+    )
 }
+
+
+
+export default TestErrorPage()
